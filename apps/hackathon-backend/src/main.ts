7cd124aa765d969a4d2427a6ba94fa9367ef2d54
Divyansh Mangla <divyansh.mangla@upgrad.com>
2025-09-06 23:31:12 +0530

    fix cors errors

--- apps/hackathon-backend/src/main.ts
+++ apps/hackathon-backend/src/main.ts
@@ -5,6 +5,13 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Enable CORS for all origins (you can restrict it if needed)
+  app.enableCors({
+    origin: '*', // allow all origins; replace with specific URLs in production
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   const port = process.env.PORT || 3005;
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}`);
